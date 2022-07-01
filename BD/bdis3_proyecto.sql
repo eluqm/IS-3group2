b4ba7221c38c923571245203e378d139fd515666
edenpl <epalli@unsa.edu.pe>
2022-06-30 22:20:37 -0500

    avance2 BD

--- BD/bdis3_proyecto.sql
+++ BD/bdis3_proyecto.sql
@@ -1,19 +1,30 @@
--- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
---
--- Host: localhost    Database: bdis3
--- ------------------------------------------------------
--- Server version	8.0.29
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+--
+-- Table structure for table `proyecto`
+--
+
+DROP TABLE IF EXISTS `proyecto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `proyecto` (
+  `id_proyecto` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `fecha` date NOT NULL,
+  `estado` varchar(45) NOT NULL,
+  `participantes` varchar(200) NOT NULL,
+  `id_usuario` varchar(45) NOT NULL,
+  `Campo_de_estudio_id_campo_de_estudio` int NOT NULL,
+  `Usuario_id_usuario` int NOT NULL,
+  `Escuela_id_escuela` int NOT NULL,
+  PRIMARY KEY (`id_proyecto`),
+  KEY `fk_Proyecto_Campo_de_estudio_idx` (`Campo_de_estudio_id_campo_de_estudio`),
+  KEY `fk_Proyecto_Usuario1_idx` (`Usuario_id_usuario`),
+  KEY `fk_Proyecto_Escuela1_idx` (`Escuela_id_escuela`),
+  CONSTRAINT `fk_Proyecto_Campo_de_estudio` FOREIGN KEY (`Campo_de_estudio_id_campo_de_estudio`) REFERENCES `campo_de_estudio` (`id_campo_de_estudio`),
+  CONSTRAINT `fk_Proyecto_Escuela1` FOREIGN KEY (`Escuela_id_escuela`) REFERENCES `escuela` (`id_escuela`),
+  CONSTRAINT `fk_Proyecto_Usuario1` FOREIGN KEY (`Usuario_id_usuario`) REFERENCES `usuario` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `proyecto`
@@ -25,12 +36,3 @@ LOCK TABLES `proyecto` WRITE;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-06-30 22:02:51
