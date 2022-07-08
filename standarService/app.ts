39dd45ea57b1d77632d848a2669831613561df37
luismoroco <lmorocoramos@gmail.com>
2022-07-08 14:51:53 -0500

    standard Microservice --test routes

--- standarService/app.ts
+++ standarService/app.ts
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv';
-import ServerForMicroservice from "./models/server";
+import ServerForMicroservice from './models/server';
 
 dotenv.config();
-const microServer = new ServerForMicroservice();
 
-microServer.listenServer();
+const iServer = new ServerForMicroservice();
+
+iServer.init();
--- standarService/models/server.ts
+++ standarService/models/server.ts
@@ -1,22 +1,55 @@
-import  express, {Application} from 'express';
+import  express, { Application } from 'express';
+import iRouter from "../routes/projects";
+import cors from 'cors';
+import iDataBase from "../db/connection";
 
 class ServerForMicroservice {
   private application: Application;
-  private port: string | number | undefined;
+  readonly port: string | number;
+  private apiPaths = {
+    projects: '/api'
+  }
 
   constructor() {
     this.application = express();
-    this.port = process.env.PORT || '3000';
+    this.port = process.env.PORT || 5000;
   }
 
-  listenServer() {
+  listenServer() : void {
     this.application.listen(
       this.port, () => {
-        console.log('Server Setup : Running in : ' + this.port);
+        console.log(`Server running in ${this.port}`);
       }
+    );
+  }
+
+  async dbConnection() : Promise<void> {
+    try {
+      await iDataBase.authenticate();
+      console.log(`Authentication server OK DataBase ${this.port}`);
+    } catch (error : any) {
+        throw new Error(error);
+    }
+  }
+
+  middlewares() : void {
+    this.application.use( cors());
+    this.application.use( express.json());
+    this.application.use( express.static('public'));
+  }
+
+  routes() {
+    this.application.use(
+      this.apiPaths.projects, iRouter
     )
+  }
+
+  init() : void {
+    this.dbConnection().then();
+    this.middlewares();
+    this.routes();
+    this.listenServer();
   }
 }
 
 export default ServerForMicroservice;
-
