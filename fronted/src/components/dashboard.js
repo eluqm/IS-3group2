80b15a749cb9e3da386c61576606b144fe8584c2
luismoroco <lmorocoramos@gmail.com>
2022-07-18 12:33:38 -0500

    front-react--login--

--- fronted/src/components/dashboard.js
+++ fronted/src/components/dashboard.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import { useHistory } from "react-router-dom";
 
 const Dashboard = () => {
+  const [name, setName] = useState('');
+  const [token, setToken] = useState('');
+
+  const refreshToken = async () => {
+    try {
+      const res = await axios.get('').then(() => alert('success')).catch(
+        (error) => console.log(`new error in ${error}`)
+      );
+    } catch (error) {
+    }
+  }
+
   return (
     <div className={'container mt-5'}>
       <h1 className={'tile'}> Bienvenido de nuevo: </h1>
